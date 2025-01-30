09fb55c048aee6f0e9d9cf80c3cf4d9eacacb067
Nizar Sbai <nizar1.sbai@gmail.com>
2025-01-30 20:21:43 +0100

    Initial commit

---
+++ src/main/java/net/javaguides/ems_backend/EmsBackendApplication.java
@@ -0,0 +1,14 @@
+package net.javaguides.ems_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Worldd");
+		SpringApplication.run(EmsBackendApplication.class, args);
+	}
+
+}

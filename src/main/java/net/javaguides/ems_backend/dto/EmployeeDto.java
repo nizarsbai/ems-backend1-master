23f68b3bbfd659c1081ed7b1885a898866ac0543
Nizar Sbai <nizar1.sbai@gmail.com>
2025-02-08 00:50:54 +0100

    getter

--- src/main/java/net/javaguides/ems_backend/dto/EmployeeDto.java
+++ src/main/java/net/javaguides/ems_backend/dto/EmployeeDto.java
@@ -6,12 +6,36 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
+//@Setter
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 public class EmployeeDto {
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
+
+    public EmployeeDto(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getter and setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {   // This method should be here
+        return email;
+    }
 }
--- src/main/java/net/javaguides/ems_backend/entity/Employee.java
+++ src/main/java/net/javaguides/ems_backend/entity/Employee.java
@@ -6,14 +6,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
+import java.util.Locale;
+
+//@Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 @Entity
 @Table(name="employees")
 public class Employee {
 
+    //Locale locale = new Locale("en","","");
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +29,29 @@ public class Employee {
 
     @Column(name = "email_id", nullable = false, unique = true)
     private String email;
+
+    public Employee(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getter and setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {   // This method should be here
+        return email;
+    }
+
 }
